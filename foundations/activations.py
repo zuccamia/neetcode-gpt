dcd6497bc2b959d84c09cecc190db615d74b44d9
Norah H. <zuccamia@gmail.com>
2026-05-17 22:02:41 -0700

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -3,16 +3,20 @@ from numpy.typing import NDArray
 
 
 class Solution:
-    
+    # Turns out Numpy's maths operations apply to entire arrays
+    # at once, instead of iterating each scalar element!
+
     def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
         # Formula: 1 / (1 + e^(-z))
         # return np.round(your_answer, 5)
-        weights_inputs = z
-        return [np.round(1 / (1 + np.exp(-i)), 5) for i in weights_inputs]
+
+        e = np.exp(-z)
+        activation = 1 / (1 + e)
+        return np.round(activation, 5)
 
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
         # Formula: max(0, z) element-wise
-        wts_ins = z
-        return [max(0.0, i) for i in wts_ins]
+
+        return np.maximum(0,z)
